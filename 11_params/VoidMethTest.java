6cfbd3f91c8712ef4a82760fddb45ff5d0da4b12
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-08-12 13:21:30 +0300

    void

--- 11_params/VoidMethTest.java
+++ 11_params/VoidMethTest.java
@@ -6,28 +6,28 @@ public class VoidMethTest {
 	static void testMeth(int param){
 		System.out.println("Step 1");
 		if(param == 1){
-			return 1;
+			System.out.println(param);
 		}
 		System.out.println("Step 2");
 		if(param == 2){
-			return 2;
+			System.out.println(param);
 		}
 		System.out.println("Step 3");
 		if(param == 3){
-			return 3;
+			System.out.println(param);
 		}
 		System.out.println("Step 4");
 		if(param == 4){
-			return 4;
+			System.out.println(param);
 		}
 		System.out.println("Step 5");
 		if(param == 5){
-			return 5;
+			System.out.println(param);
 		}
 		
 		System.out.println("Step 6");
 		if(param == 6){
-			return 6;
+			System.out.println(param);
 		}
 	}
 }
