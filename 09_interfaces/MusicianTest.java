60b670a56f5e01caafd2afecc3c55a0fb41fd3b4
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-08-27 13:57:16 +0300

    interfaces

---
+++ 09_interfaces/MusicianTest.java
@@ -0,0 +1,10 @@
+public class MusicianTest {
+	public static void main(String[] args) {
+		Musician [] musicians = new Musician []{new PuffDaddy(), new JohnLegend(), new Snoop(), new Stanley()};
+		
+		for(int i = 0;i<musicians.length;i++){
+			System.out.println("Name : "+ musicians[i].getName());
+			System.out.println("Lyrics : "+ musicians[i].getLyrics());
+		}
+	}
+}
---
+++ 09_interfaces/Stanley.java
@@ -0,0 +1,10 @@
+public class Stanley implements Musician {
+	public String getLyrics() {
+		return "Anguka nayo";
+	}
+
+	public String getName() {
+		return "Stanley";
+	}
+
+}
