e4fb4f829a49cc7f55e6d584551c652b207dba5b
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-08-28 11:50:58 +0300

    initializer

---
+++ 08_initializers/InitializerTest.java
@@ -0,0 +1,11 @@
+public class InitializerTest {
+	static {
+		new Initializer();
+		new Initializer();
+		new Initializer();
+	}
+
+	public static void main(String[] args) {
+		//new Initializer();
+    }
+}
--- 09_interfaces/MusicianTest.java
+++ 09_interfaces/MusicianTest.java
@@ -8,3 +8,6 @@ public class MusicianTest {
 		}
 	}
 }
+
+
+
