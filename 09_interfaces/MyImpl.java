f2826310474ec9fdb9fcc86156bf5063b9a04385
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-09-13 18:31:56 +0300

    interfaces

---
+++ 09_interfaces/MyImpl.java
@@ -0,0 +1,15 @@
+public class MyImpl implements ParentInt, ChildInt {
+
+	public double getParentsMoney() {
+		return 150.00;
+	}
+
+	public double getGrandParentsMoney() {
+		return 100.00;
+	}
+
+	public double getChildsMoney() {
+		return 50.00;
+	}	
+
+}
