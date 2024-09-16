1cd47318bb3b57dee60829779c31a992a41244ed
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-09-16 11:48:16 +0300

    abstract

---
+++ 24_abstract/AbstractTest1.java
@@ -0,0 +1,9 @@
+public class AbstractTest1 extends AbstractTest {
+	
+	public void doSomething() {
+		doConcrete1();
+		System.out.println("Doing something");
+		doConcrete2();
+	}
+
+}
---
+++ 24_abstract/AbstractTestMain.java
@@ -0,0 +1,6 @@
+public class AbstractTestMain {
+	public static void main(String[] args) {
+		AbstractTest1 test = new AbstractTest1();
+		test.doSomething();
+	}
+}
