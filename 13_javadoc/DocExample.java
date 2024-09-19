42071b97db52bd9228030e5a0914d8de82770409
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-09-20 00:38:29 +0300

    javadoc

--- 13_javadoc/DocExample.java
+++ 13_javadoc/DocExample.java
@@ -14,4 +14,20 @@ public class DocExample {
     public int getVar(){
         return var;
     }  
+
+    /**
+     * Creates a concatenation of name and age and returns the result
+     *  
+     * @throws NoSuchElementException Indicates that the name is invalid
+     * @param name Name of person
+     * @param age Age of person
+     * @return return name and age.
+     * @see DocExampleLink#getVar()
+     * @since 1.4
+     * @deprecated Do not use this class use {@link DocExampleLink} instead
+     * */
+    public String doConcat(String name, int age)throws NoSuchElementException{
+        return name+" "+age;
+    }  
+    
 }
