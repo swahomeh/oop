8099f88458304c1cff147f98aae193b61dbeab06
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-09-20 00:14:04 +0300

    javadoc

---
+++ 13_javadoc/DocExample.java
@@ -0,0 +1,17 @@
+/**A Class that shows an example of javadoc
+ * @author Mr Dude
+ * @version 1.3
+ * @see DocExampleLink#getVar()
+ * @since 1.1
+ * @deprecated Do not use this class use {@link DocExampleLink} instead
+ */
+public class DocExample {
+	
+    /**Variable javadoc*/
+    protected int var;
+	
+    /**Method level javadoc*/
+    public int getVar(){
+        return var;
+    }  
+}
