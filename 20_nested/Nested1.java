e9ca53223a13f9a41087632d1fb02bad6bd2dadc
T1TAN-X <25081164+swahomeh@users.noreply.github.com>
2024-10-02 12:13:31 +0300

    nested

---
+++ 20_nested/Nested1.java
@@ -0,0 +1,13 @@
+public class Nested1 {
+	class Inner{
+		public Inner ()	{
+			System.out.println("Hello. I am constructing...");
+		}
+
+	}
+
+	public static void main (String[] args) {
+		Nested1 outer = new Nested1();
+		Inner inner = outer.new Inner();
+	}
+}
